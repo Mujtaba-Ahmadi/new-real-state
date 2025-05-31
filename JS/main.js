e63af5a22431fb036f2ebc21a5ec093a32eb13c0
Mujtaba Ahmadi <mujtabaahmadi719@gmail.com>
2025-05-31 20:15:17 +0430

    Navbar added

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+ ScrollReveal().reveal('.text', {
+        duration: 1500,
+        easing:'ease-in-out',
+        origin:"bottom",
+        distance:'25px'
+
+    });
+
+
+ ScrollReveal().reveal('.service-container', {
+        duration: 1500,
+        easing:'ease-in-out',
+        origin:"left",
+        distance:'25px'
+
+    });
+ ScrollReveal().reveal('.house', {
+        delay: 200,
+        duration: 1500,
+        easing:'ease-in-out',
+        origin:"left",
+        distance:'25px'
+        
+    });
+    ScrollReveal().reveal('.flat', {
+        delay: 500,
+        duration: 1500,
+        easing:'ease-in-out',
+        origin:"bottom",
+        distance:'30px'
+
+    });
+ ScrollReveal().reveal('.villa', {
+        delay: 1000,
+        duration: 1500,
+        easing:'ease-in-out',
+        origin:"top",
+        distance:'40px'
+
+    });
